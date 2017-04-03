4a3e90c9841d1a60806f2716b519efd3c5c83337
Corey Salzer <coreysalzer@gmail.com>
2017-04-03 13:08:59 -0500

    Initial Commit

---
+++ InABlink/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InABlink
+//
+//  Created by Corey Salzer on 4/3/17.
+//  Copyright © 2017 Washington University in St. Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
